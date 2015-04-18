d1d6bb57a116e02b3a0c1253ed30fcf820a01ea4
mktanksley <mktanksley@gmail.com>
2015-04-18 12:20:07 -0500

    Update IncremintyMain.java

--- IncremintyMain.java
+++ IncremintyMain.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 public class IncremintyMain {
 	public static void main(String[] args) {
+		//here's our scanner
+		Scanner scnr = new Scanner(System.in);
+		//variables
+		String userEntry = "";
 		
+		System.out.println("Please enter an integer:");
+		userEntry = scnr.next();
+		System.out.println("You entered: " + userEntry);
 	}
 }
