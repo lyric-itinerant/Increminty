0cbcc7b4542dbdcf0c99b1c99dd9860b950c1619
mktanksley <mktanksley@gmail.com>
2015-04-19 11:21:10 -0500

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,18 @@
 public class test {
 	public static void main(String[] args) {
 //test main method to make sure calling the other methods works juuuust fine
-		int number = 1234;
+		int number = -123456789;
+		int newNumber;
 		IntArray intArray = new IntArray(number);
+		
+		//call different methods depending on if the number is negative or not
 		if (number >= 0 ) {
-			System.out.println(ReCurses.ReCurse(intArray.disArray()));
-		} else if (number < 0) {
-			System.out.println(ReCurses.ReCurseNeg(intArray.disArray()));
+			newNumber = ReCurses.ReCurse(intArray.disArray());
+		} else {
+			newNumber = ReCurses.ReCurseNeg(intArray.disArray());
 		}
+
+		System.out.println(Stringify.StringifyIt(newNumber));
 
 	}
 }
